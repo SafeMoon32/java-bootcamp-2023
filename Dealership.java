fcab05e34b4c6967108c8eed04c7c2fde1de08c2
Gerald MacMurray <gerald.macmurray@penske.com>
2023-01-10 14:43:47 -0500

    created project 1/10/23
    hello world

---
+++ Dealership.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Dealership {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the MacMurray Dealership");
+
+        System.out.println(" - Select option 'a' to buy a car");
+        System.out.println(" - Select option 'b' to sell a car");
+        String option = scan.nextLine();
+        switch(option) {
+            case "a":
+                System.out.println("What is your budget?");
+                int budget = scan.nextInt();
+                if (budget >= 10000) {
+                    System.out.println("Great! A Nissan Altima is available.");
+                    System.out.println("\nDo you have insurance? Write 'yes' or 'no'");
+                    scan.nextLine();
+                    String insurance = scan.nextLine();
+                    System.out.println("\nDo you have a Driver license? Write 'yes' or 'no'");
+                    String license = scan.nextLine();
+                    System.out.println("\nWhat is your credit score?");
+                    int creditScore = scan.nextInt();
+                    if(insurance.equals("yes") && license.equals("yes") && creditScore > 660){
+                        System.out.println("Sold! Pleasure doing business with you.");
+                    } else {
+                        System.out.println("We're sorry. You are not eligible");}
+                } else {
+                    System.out.println("We do not sell cars under $10,000.00. Sorry!");
+                }
+                break;
+
+            case "b":
+                System.out.println("What is your car valued at?");
+                int value = scan.nextInt();
+                System.out.println("What is your selling price?");
+                int price = scan.nextInt();
+
+                if (value > price && price < 30000) {
+                    System.out.println("We will buy your car, it's been a pleasure doing business with you");
+                } else {
+                    System.out.println("Sorry, we're not interested");
+                }
+                break;
+            default:
+                System.out.println("Invalid option dummy");
+        }
+        scan.close();
+    }
+}
