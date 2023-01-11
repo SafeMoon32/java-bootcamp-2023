b8bbe457108b196317f1e6cb3c59d86fd8947292
Gerald MacMurray <gerald.macmurray@penske.com>
2023-01-11 11:27:20 -0500

    Tip
    Wrote a program that will calculate the amount due for a tip at 15% of the bill.

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.2 Solution/Tip.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.2 Solution/Tip.java
@@ -2,7 +2,7 @@ public class Tip {
 
     public static void main(String[] args) {
         double bill = 53.5;
-        System.out.println("Waiter: I hope you enjoyed your meal!");
+        System.out.println("Waiter: e");
 
         
         System.out.println("Thank you!");
