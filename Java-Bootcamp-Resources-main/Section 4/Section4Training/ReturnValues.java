a4ce1fc36dbc71f1c3341b7ef06b5448342a81f9
Gerald MacMurray <gerald.macmurray@penske.com>
2023-01-11 14:05:31 -0500

    Functions
    Created two functions that RETURN values.

--- Java-Bootcamp-Resources-main/Section 4/Section4Training/ReturnValues.java
+++ Java-Bootcamp-Resources-main/Section 4/Section4Training/ReturnValues.java
@@ -5,10 +5,35 @@ public class ReturnValues {
         double area1 = calculateArea(2.3, 3.5);
         double area2 = calculateArea(1.6, 2.4);
         double area3 = calculateArea(2.3, 3.5);
+
+        calculateArea(-5, 2.3);
+
+        String englishExplanation = explainArea("English");
+        String frenchExplanation = explainArea("French");
+        String spanishExplanation = explainArea("Spanish");
+        String italianExplanation = explainArea("Italian");
+
     }
     public static double calculateArea(double length, double width) {
+        if (length < 0 || width < 0) {
+            System.out.println("INVALID DIMENSIONS");
+            System.exit(0);
+        }
         double area = length * width;
         return area;
     }
+
+    public static String explainArea(String language){
+        switch(language) {
+            case "English": return "Area equals length * width";
+            case "French": return "La surface est egale a la longueur * la largeur";
+            case "Spanish": return "area es igual a largo * ancho";
+            default: return "Language not available";
+        }
+    }
 }
- 
+//        English "Area equals length * width"
+//
+//        French "La surface est egale a la longueur * la largeur"
+//
+//        Spanish "area es igual a largo * ancho"
