88b6ab8e4d67fa7ed3164bc2d18a1d17c009c220
Gerald MacMurray <gerald.macmurray@penske.com>
2023-01-11 09:11:53 -0500

    Detention
    Remember instead of copying and pasting your code, put it in a method and then call that method from the function within Main.

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.1/Detention.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/4. Functions/Workbook 4.1/Detention.java
@@ -1,14 +1,20 @@
 public class Detention {
     public static void main(String[] args) {
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+    }
+        public static void printLines() {
 
         //what Bart did so far
-        System.out.println("I will not copy and paste code.");
-        System.out.println("I will use Camel Case when writing class names.");
-        System.out.println("I will use lower Camel Case when writing function names.");
-        System.out.println("I will use lower Camel Case when writing variables names.\n");
-
-        // Task 2 – call the function 6 times. 
-   }
+            System.out.println("I will not copy and paste code.");
+            System.out.println("I will use Camel Case when writing class names.");
+            System.out.println("I will use lower Camel Case when writing function names.");
+            System.out.println("I will use lower Camel Case when writing variables names.\n");
+        }
+    }
 
 
 
@@ -22,4 +28,5 @@ public class Detention {
       */
 
 
-}
+
+
