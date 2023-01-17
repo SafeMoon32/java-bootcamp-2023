685372e72c49a5e4db8554e474fb28c1faba489f
Gerald MacMurray <gerald.macmurray@penske.com>
2023-01-17 16:19:31 -0500

    DiceJack
    This commit has added the println's to print the players rolls, then added the if statements showing that if it was less than zero, or greater than 8 it would then print that the number chosen does not work and then it would shutdown the game.

--- Java-Bootcamp-Resources-main/Section 4/Section4Training/DiceJack.java
+++ Java-Bootcamp-Resources-main/Section 4/Section4Training/DiceJack.java
@@ -1,19 +1,38 @@
 package Section4Training;
 
+import java.util.Scanner;
+
 public class DiceJack {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
         int roll1 = rollDice();
         int roll2 = rollDice();
         int roll3 = rollDice();
 
-        System.out.println(roll1);
-        System.out.println(roll2);
-        System.out.println(roll3);
+        System.out.println("Please enter number between One & Six");
+
+
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        int num3 = scan.nextInt();
+
+        if(num1 < 1 || num2 < 1 || num3 < 1) {
+            System.out.println("Numbers can not be less than 1. Shutting the game down!");
+        System.exit(0);
+
+        }
+        if(num1 > 6 || num2 > 6 || num3 > 6)
+            System.out.println("Numbers cannot be higher than 6. Shutting the game down!");
+        System.exit(0);
+        scan.close();
     }
     public static int rollDice() {
                                 //0 - 0.9999999999
         double randomNumber = Math.random() * 6;
         randomNumber += 1; //1 - 6.99999999999
         return(int)randomNumber;
+
+
     }
 }
